c1a3504475b64ee6ef243a4d96ab6f547c6147a7
sam0_0sam6 <sam0_0sam6@hotmail.com>
2020-05-13 02:14:17 +0300

    first commit

---
+++ WEBSERVICE/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WEBSERVICE
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("HUSSAIN!");
+        }
+    }
+}
